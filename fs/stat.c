c7b0307847a5ef912c111390454e878e80a531d9
Christian Brauner <brauner@kernel.org>
2024-04-30 13:57:58 +0200

    stat: use vfs_empty_path() helper
    commit 27a2d0c upstream.
    
    Use the newly added helper for this.
    
    Signed-off-by: Christian Brauner <brauner@kernel.org>
    Signed-off-by: Miao Wang <shankerwangmiao@gmail.com>

--- fs/stat.c
+++ fs/stat.c
@@ -222,16 +222,9 @@ int vfs_fstatat(int dfd, const char __user *filename,
 	 * If AT_EMPTY_PATH is set, we expect the common case to be that
 	 * empty path, and avoid doing all the extra pathname work.
 	 */
-	if (dfd >= 0 && flags == AT_EMPTY_PATH) {
-		char c;
+	if (flags == AT_EMPTY_PATH && vfs_empty_path(dfd, filename))
+		return vfs_fstat(dfd, stat);
 
-		ret = get_user(c, filename);
-		if (unlikely(ret))
-			return ret;
-
-		if (likely(!c))
-			return vfs_fstat(dfd, stat);
-	}
 	return vfs_statx(dfd, filename, flags | AT_NO_AUTOMOUNT,
 			 stat, STATX_BASIC_STATS);
 }
