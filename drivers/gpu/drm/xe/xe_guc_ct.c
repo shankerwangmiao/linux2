AOSC-Tracking/linux

[LOONGARCH64] drm/xe: fix build on non-4K kernels
This is required to make xe work on LoongArch, which defaults to 16K pages.

drivers/gpu/drm/xe/xe_guc_ct.c
@@ -143,7 +143,7 @@ int xe_guc_ct_init(struct xe_guc_ct *ct)
 	struct xe_bo *bo;
 	int err;
 
-	xe_assert(xe, !(guc_ct_size() % PAGE_SIZE));
+	xe_assert(xe, !(guc_ct_size() % XE_PAGE_SIZE));
 
 	drmm_mutex_init(&xe->drm, &ct->lock);
 	spin_lock_init(&ct->fast_lock);

shankerwangmiao/linux2

Ref: Contact the author.

drivers/gpu/drm/xe/xe_guc_ct.c
@@ -145,7 +145,7 @@ int xe_guc_ct_init(struct xe_guc_ct *ct)
 	struct xe_bo *bo;
 	int err;
 
-	xe_gt_assert(gt, !(guc_ct_size() % PAGE_SIZE));
+	xe_gt_assert(gt, !(guc_ct_size() % XE_PAGE_SIZE));
 
 	ct->g2h_wq = alloc_ordered_workqueue("xe-g2h-wq", 0);
 	if (!ct->g2h_wq)